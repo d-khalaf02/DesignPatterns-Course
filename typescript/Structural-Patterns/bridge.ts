d354ae270326d8af7fd218ac262417ec7fb827b1
Diyar Khalaf <d.khalaf@xsigns.de>
2024-09-13 08:32:17 +0200

    Renaming files...

---
+++ typescript/Structural-Patterns/bridge.ts
@@ -0,0 +1,47 @@
+interface MediaPlayerImplementation{
+    playAudio(): void;
+    playVideo(): void;
+}
+
+class WindowsMediaPlayer implements MediaPlayerImplementation {
+    playAudio() {
+        console.log('Playing Audio on Windows...')
+    }
+    playVideo() {
+        console.log('Playing Video on Windows...')
+    }
+}
+
+class MacMediaPlayer implements MediaPlayerImplementation {
+    playAudio() {
+        console.log('Playing Audio on Mac OS...')
+    }
+    playVideo() {
+        console.log('Playing Video on Mac OS...')
+    }
+}
+
+// Bridge Pattern
+abstract class MediaPlayerAbstraction{
+    constructor(protected implementation: MediaPlayerImplementation){}
+
+    abstract playFile(): void;
+}
+
+class AudioPlayer extends MediaPlayerAbstraction{
+    playFile() {
+        this.implementation.playAudio();
+    }
+}
+
+class VideoPlayer extends MediaPlayerAbstraction{
+    playFile() {
+        this.implementation.playVideo();
+    }
+}
+
+const audioPlayer = new AudioPlayer(new WindowsMediaPlayer());
+audioPlayer.playFile();
+
+const videoPlayer = new VideoPlayer(new MacMediaPlayer())
+videoPlayer.playFile();
